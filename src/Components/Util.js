13e627114c1435f2747631cbee53735ba65ed9dc
reutleib <reut@greeninvoice.co.il>
2019-03-28 20:09:23 +0200

    not working yet

--- src/Components/Util.js
+++ src/Components/Util.js
@@ -1,7 +1,3 @@
-import { func } from 'prop-types';
-
-var Promise = require('es6-promise').Promise;
-
 export function load_google_maps(){ 
     return new Promise(function(resolve,reject){
         window.resolveGoogleMapsPromise = function() {
@@ -12,18 +8,18 @@ export function load_google_maps(){
 
         // now load the google maps api
         const script = document.createElement("script");
-        const API_KEY = 'AIzaSyAe-QNZbHPDbiEuWy9IHJsi5l6yJUJ_7hM';
+        const API_KEY = 'AIzaSyAHjuSuRkHIU84dbtT8c1iDRUCIxqRLhRc';
         script.src = `https://maps.googleapis.com/maps/api/js?libraries=places&key=${API_KEY}&callback=resolveGoogleMapsPromise`;
         script.async = true;
         document.body.appendChild(script);
     });
 }
 
-// export function load_places(){
-//     let city= "Silver Spring, MD";
-//     let query = 'Shopping';
-//     var api_url = `https://api.foursquare.com/v2/venues/search?client_id=N1IAMKZUIK1AUHKRFGFBKPQ2YKDSBAKS4NTER5SYZN5CROR1&client_secret=4MKLXVLU2FGZQVRMAEDC15P0TFJGSCY3ZUYUZ0KHQQQLQ5R3&v=20130815%20&limit=50&near=${city}&query=${query}`;
+export function load_places(){
+    let city= "Silver Spring, MD";
+    let query = 'Shopping';
+    var api_url = `https://api.foursquare.com/v2/venues/search?client_id=N1IAMKZUIK1AUHKRFGFBKPQ2YKDSBAKS4NTER5SYZN5CROR1&client_secret=4MKLXVLU2FGZQVRMAEDC15P0TFJGSCY3ZUYUZ0KHQQQLQ5R3&v=20130815%20&limit=50&near=${city}&query=${query}`;
 
-//     return fetch(api_url).then(resp=>resp.json());
+    return fetch(api_url).then(resp=>resp.json());
 
-// }
+}
