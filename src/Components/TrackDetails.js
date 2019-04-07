560a31a1e03b633ad49c3d9a68b8ee5245386273
reutleib <reut@greeninvoice.co.il>
2019-04-07 20:44:04 +0300

    b

--- src/Components/TrackDetails.js
+++ src/Components/TrackDetails.js
@@ -6,6 +6,7 @@ import TiBackspace from 'react-icons/lib/ti/backspace';
 import IoAndroidBicycle from 'react-icons/lib/io/android-bicycle';
 import MdDirectionsWalk from 'react-icons/lib/md/directions-walk';
 import HomePage from './HomePage';
+import $ from 'jquery';
 import './style/TrackDetails.css'
 
 
@@ -24,6 +25,7 @@ class TrackDetails extends Component {
     this.viewTrack = this.viewTrack.bind(this)
     this.updateTrack = this.updateTrack.bind(this)
     this.getComments = this.getComments.bind(this)
+    // this.rating = this.rating.bind(this)
   }
   
   componentDidMount() {
@@ -126,6 +128,62 @@ class TrackDetails extends Component {
      
   }
 
+  // rating(){
+  
+  //     /* 1. Visualizing things on Hover - See next part for action on click */
+  //     $('#stars li').on('mouseover', function(){
+  //       var onStar = parseInt($(this).data('value'), 10); // The star currently mouse on
+       
+  //       // Now highlight all the stars that's not after the current hovered star
+  //       $(this).parent().children('li.star').each(function(e){
+  //         if (e < onStar) {
+  //           $(this).addClass('hover');
+  //         }
+  //         else {
+  //           $(this).removeClass('hover');
+  //         }
+  //       });
+        
+  //     }).on('mouseout', function(){
+  //       $(this).parent().children('li.star').each(function(e){
+  //         $(this).removeClass('hover');
+  //       });
+  //     });
+      
+      
+  //     /* 2. Action to perform on click */
+  //     $('#stars li').on('click', function(){
+  //       var onStar = parseInt($(this).data('value'), 10); // The star currently selected
+  //       var stars = $(this).parent().children('li.star');
+        
+  //       for (let i = 0; i < stars.length; i++) {
+  //         $(stars[i]).removeClass('selected');
+  //       }
+        
+  //       for (let i = 0; i < onStar; i++) {
+  //         $(stars[i]).addClass('selected');
+  //       }
+        
+  //       // JUST RESPONSE (Not needed)
+  //       var ratingValue = parseInt($('#stars li.selected').last().data('value'), 10);
+  //       var msg = "";
+  //       if (ratingValue > 1) {
+  //           msg = "Thanks! You rated this " + ratingValue + " stars.";
+  //       }
+  //       else {
+  //           msg = "We will improve ourselves. You rated this " + ratingValue + " stars.";
+  //       }
+  //       responseMessage(msg);
+        
+  //     });
+      
+  //   function responseMessage(msg) {
+  //     $('.success-box').fadeIn(200);  
+  //     $('.success-box div.text-message').html("<span>" + msg + "</span>");
+  //   }
+
+  // }
+
   viewTrack(track,i) {
     console.log("TRACKKKKKKKKKKK _____________________");
     console.log(track);
@@ -155,6 +213,8 @@ class TrackDetails extends Component {
              <p style={{ border:`groove`,fontSize:'10px'}}>{this.getComments(track.comments)}</p>
              
             <div>
+              <p className="titles">comments: </p>
+
             </div>
           </TamplateComponent>
           <div>
