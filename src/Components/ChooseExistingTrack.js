dd27117cda6aca25e2dea9af08842b141af11583
reutleib <reut@greeninvoice.co.il>
2019-04-28 12:02:30 +0300

    choose

--- src/Components/ChooseExistingTrack.js
+++ src/Components/ChooseExistingTrack.js
@@ -206,48 +206,186 @@ class ChooseExistingTrack extends Component {
   render() {
     return (
       <div>
+        <Card className="text-center">
 
-        
-          <form onSubmit={this.onSubmit}>
-            <div className="row">
-              
-            <div className="col bg-white rounded">
-              <label>From city:
-              <input required className="mt-2 form-control float-left" type="text" name="from" onChange={this.handleChange} value={this.state.from}/>
-            </label>
-            <label>To city:
-              <input required className="mt-2 form-control float-left" type="text" name="to" onChange={this.handleChange} value={this.state.to}/>
-            </label>
+          <Card.Header>
+            <Navbar collapseOnSelect expand="lg">
+
+              <Navbar.Brand href="#profilePicture" style={{ float: 'left' }}>
+                {this.state.userDetails.profilePicture ?
+                  (
+                    <img alt="Profile" src={this.state.userDetails.profilePicture} style={{ height: '40px', width: '40px', float: 'left', borderRadius: '50%' }}></img>
+                  )
+                  :
+                  (
+                    <div className='sweet-loading'> <BeatLoader color={'#123abc'} /> </div>
+                  )
+                }
+              </Navbar.Brand>
+
+              <Navbar.Brand href="#name" style={{ float: 'center' }}>
+                {this.state.userDetails.name ?
+                  (
+                    <div>
+                      <p>{this.state.userDetails.name}</p>
+                    </div>
+                  )
+                  :
+                  (
+                    <div className='sweet-loading'> <BeatLoader color={'#123abc'} /> </div>
+                  )
+                }
+              </Navbar.Brand>
+
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                  <Nav.Link href="#profile">View Profile</Nav.Link>
+                  <Nav.Link href="#favoriteTracks">Favorite Tracks</Nav.Link>
+                  <NavDropdown title="Navigate a Route" id="collasible-nav-dropdown">
+                    <NavDropdown.Item href="#action/2.1">Choose Existing Track</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/2.2">Generate Auto Track</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/2.3">Custom Made Track</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/2.4">Info</NavDropdown.Item>
+                  </NavDropdown>
+                  <Nav.Link href="#searcgTracks">Serach Tracks</Nav.Link>
+                  <Nav.Link href="#vibrations">Vibrations</Nav.Link>
+                  <Nav.Link href="#about">About</Nav.Link>
+                  <Nav.Link href="#contact">Contact us</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+
+            </Navbar>
+          </Card.Header>
+
+          <Card.Body>
+            <Card.Title>
+           </Card.Title>
+
+           <form onSubmit={this.onSubmit}>
+           <div className="row">
+             
+           <div className="col bg-white rounded">
+             <label>From city:
+             <input required className="mt-2 form-control float-left" type="text" name="from" onChange={this.handleChange} value={this.state.from}/>
+           </label>
+           <label>To city:
+             <input required className="mt-2 form-control float-left" type="text" name="to" onChange={this.handleChange} value={this.state.to}/>
+           </label>
+         </div>
+
+         <div className="col bg-white rounded">
+             <label>Type:</label>
+             <span className="d-block">
+             <input className="float-left" type="radio" ref="walking" name="type" id="walking" autocomplete="off" onChange={this.handleChange} value={this.state.walking} required />
+               <MdDirectionsWalk /> 
+             </span>
+             <span className="d-block">
+             <input className="float-left" type="radio" ref="bicycling" name="type" id="bicycling" autocomplete="off" onChange={this.handleChange} value={this.state.bicycling} />
+               <IoAndroidBicycle /> 
+             </span>
+             
+         </div> 
+       
+         <div className="w-100 mb-md-4"></div>
+         <div className="col-12 mx-auto">
+            <button className='btn btn-primary' type='submit'>
+              Build Route
+            </button>
           </div>
 
-          <div className="col bg-white rounded">
-              <label>Type:</label>
-              <span className="d-block">
-              <input className="float-left" type="radio" ref="walking" name="type" id="walking" autocomplete="off" onChange={this.handleChange} value={this.state.walking} required />
-                <MdDirectionsWalk /> 
-              </span>
-              <span className="d-block">
-              <input className="float-left" type="radio" ref="bicycling" name="type" id="bicycling" autocomplete="off" onChange={this.handleChange} value={this.state.bicycling} />
-                <IoAndroidBicycle /> 
-              </span>
-              
-          </div> 
-        
-          <div className="w-100 mb-md-4"></div>
-          <div className="col-12 mx-auto">
-            <button type="submit" className=" btn btn-primary mt-2 tn-block rounded-0 w-100 border-0" style={{backgroundColor:'black', color:'#68e8db'}}>search tracks</button>
-          </div>
+           </div>
+         </form>
 
-            </div>
-          </form>
-            <div className="w-100 mb-md-4 pt-3"></div>
+          </Card.Body>
+
+          <Card.Header> 
+          </Card.Header>
+
+
+          <div className="w-100 mb-md-4 pt-3"></div>
             <div className="col-12 mx-auto">
                 {this.state.tracks.map(this.viewTracks)}
             </div>
-        </div>
+
+            
+          <Card.Body>
+            {this.state.loading ?
+              (
+            <div className="col-12 mx-auto">
+                {this.state.tracks.map(this.viewTracks)}
+            </div>
+              )
+              :
+              (
+                <div className='sweet-loading'> <BeatLoader color={'#123abc'} /> </div>
+              )
+            }
+          </Card.Body>
+          <Card.Footer id="locationUpdate" className="text-muted"></Card.Footer>
+        </Card>
+      </div>
     );
+
   }
 }
 
 
 export default ChooseExistingTrack;
+
+// <input className="float-left" type="radio" name="option" id="walking" autocomplete="off" checked onChange={this.handleChange} value={this.state.type1} /> 
+// 
+
+// <input className="float-left" type="checkbox" ref="walking" name="walking" onChange={this.onChange} value={this.state.walking} aria-label="..."/>
+// <input className="float-left" type="checkbox" ref="bicycling" name="bicycling" onChange={this.onChange} value={this.state.bicycling} aria-label="..."/>
+
+// <label> Difficulty level:
+//               <input required className="mt-2 form-control float-left" type="number" name="difficulty" min="1" max="5" onChange={this.handleChange} value={this.state.difficulty}></input>
+//             </label>
+
+// <input className="float-left" type="radio" ref="walking" name="walking" autocomplete="off" onChange={this.handleChangeRadio} value={this.state.walking} /> 
+// <input className="float-left" type="radio" ref="bicycling" name="bicycling" autocomplete="off" onChange={this.handleChangeRadio} value={this.state.bicycling} /> 
+
+
+
+
+
+// viewTracks(track,i) {
+//   if(track.title == ''){
+//     console.log("there are no tracks to display !");
+//     return (
+//       <div>
+//         <h3 style={{ margin: '0 auto'}}> There are no tracks to display</h3>
+//       </div>
+//     )
+//   }
+//   else{
+   
+//     return (          
+//       <div key={'container'+i} className="col-10 p-md-4 card" style={{ margin:`0 auto`,width: 18 + 'rem'}}>
+//           <div className="">
+//             <TamplateComponent key={'track'+i} index={i} onChange={this.updateTracks}>  
+            
+//             <NavLink to=
+//             //navigate to TrackDetails via TemplateComponent with the params
+//             {{pathname: `${process.env.PUBLIC_URL}/trackDetails`, 
+//               idOfTrack: track.idOfTrack}}
+//               activeStyle={this.active} 
+//               className="" >
+//               <h1 className="card-title" style={{ textAlign:`center`}}>{track.title} {this.getIconType(track.type)}</h1>
+//               <p style={{ textAlign:`center`}}>Desription: <br></br>{track.description}</p>
+//             </NavLink>
+
+//             <div>
+//               <p>comments: </p>
+//               <p style={{ border:`groove`,fontSize:'10px'}}>{this.getComments(track.comments)}</p>
+//             </div>
+            
+//             </TamplateComponent>
+//         </div>
+        
+//       </div>
+//     )
+//   }
+// }
