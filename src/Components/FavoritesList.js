2a295fd2065b7f3e50a5c671128fd9c7caea8d5f
Roni Polisanov <yyyroni@gmail.com>
2019-06-05 11:42:54 +0300

    Some fixes

--- src/Components/FavoritesList.js
+++ src/Components/FavoritesList.js
@@ -12,9 +12,7 @@ class FavoriteTracks extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      variant: 'success',
       loading: true,
-      tracks: [],
       userDetails: [],
       show: false,
       isUpdated: false
@@ -67,48 +65,37 @@ class FavoriteTracks extends Component {
           <Breadcrumb.Item active>Favorite Tracks</Breadcrumb.Item>
         </Breadcrumb>
         <CardGroup>
-        <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
-          <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-              This is a wider card with supporting text below as a natural lead-in to
-              additional content. This content is a little bit longer.
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
-        </Card>
-        <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
-          <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-              This card has supporting text below as a natural lead-in to additional
-              content.{' '}
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
-        </Card>
-        <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
-          <Card.Body>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-              This is a wider card with supporting text below as a natural lead-in to
-              additional content. This card has even longer content than the first to
-              show that equal height action.
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
-        </Card>
+
+        {!this.state.loading && userDetails.trackRecords.map( (track, index) => {
+          return(
+            <Card key={track}>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>{track.title}</Card.Title>
+                <Card.Text>
+                  {track.startPoint}
+                  {track.endPoint}
+                  {track.wayPoints}
+                  {track.reports}
+                  {track.travelMode}
+                  {track.distance}
+                  {track.description}
+                  {track.difficultyLevel}
+                  {userDetails.accessibility === 1 ? track.nonDisabledTime : track.disabledTime}
+                  {track.description}
+                  This is a wider card with supporting text below as a natural lead-in to
+                  additional content. This card has even longer content than the first to
+                  show that equal height action.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+          );
+        })}
       </CardGroup>
-        { !this.state.loading && userDetails.trackRecords.map(this.trackRecord)}
+        
       </div>
     );
   }
