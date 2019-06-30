ab87a95012eefada202433c60a7eac477fc82eb9
reutleib <reut@greeninvoice.co.il>
2019-06-30 12:50:41 +0300

    changes

--- src/router/router.js
+++ src/router/router.js
@@ -1,11 +1,9 @@
 import React from "react";
 import {Route} from "react-router-dom";
-import Header from "../Header";
 import LiveNavigation from "../Components/LiveNavigation";
 import AutoGenerateTrack from "../Components/AutoGenerateTrack";
 import ChooseExistingTrack from "../Components/ChooseExistingTrack";
 import CustomTrack from "../Components/CustomTrack";
-// import StartNavigate from "../Components/StartNavigate";
 import Login from "../Components/Login";
 import Map from "../Components/Map";
 import TrackDetails from "../Components/TrackDetails";
@@ -14,14 +12,6 @@ import PostNavigation from '../Components/PostNavigation'
 import Profile from "../Components/Profile";
 import Favorites from "../Components/FavoritesList";
 import LiveMap from "../Components/LiveMap";
-// import DirectionsPoint from "../Components/DirectionsPoints";
-// import Navigate from "../Components/Navigate";
-
-// import UserSettings from "../Components/UserSettings";
-// import Disconnect from "../Components/Disconnect";
-// import BuildTrack from "../Components/BuildTrack";
-// import BrowseTrack from "../Components/BrowseTrack";
-
 
 const AppRouter = () =>{
   return(
@@ -41,17 +31,7 @@ const AppRouter = () =>{
       <Route exact path="/liveMap" component={LiveMap}/>
     </React.Fragment>
   );
-// <Route exact path="/" component={Navigate} />
-// <Route exact path="/" component={UserProfile} />
-// <Route exact path="/" component={UserSettings} />
-// <Route exact path="/" component={Disconnect} />
-// <Route exact path="/" component={BuildTrack} />
-// <Route exact path="/" component={BrowseTrack} />
+  
 }
-
-// <Route exact path="/" component={SomePage} />
-// <Route exact path="/custom" component={CustomTrack} />
-// <Route exact path="/start" component={StartNavigate} />      
-// <Route exact path="/directions" component={DirectionsPoint} />
 
 export default AppRouter;
