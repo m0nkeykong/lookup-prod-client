5839d19d8489f5192a16d6aa0846b8766934d3c7
m0nkeykong <haim1321@gmail.com>
2019-08-30 14:56:09 +0300

    updated favoriteList css

--- src/Components/FavoritesList.js
+++ src/Components/FavoritesList.js
@@ -30,7 +30,7 @@ class FavoriteTracks extends Component {
   }
 
   // Fetch the updated user data from db
-  getUserDetails(){
+  getUserDetails() {
     var self = this;
     return new Promise(resolve => {
       self.userid = JSON.parse(sessionStorage.getItem('userDetails'));
@@ -42,72 +42,72 @@ class FavoriteTracks extends Component {
         })
         .catch(error => {
           console.error(error);
-        });      
+        });
     });
   }
 
-  async componentDidMount(){
+  async componentDidMount() {
     this.userid = JSON.parse(sessionStorage.getItem('userDetails'));
     console.log(`Entered <FavoriteList> componentDidMount(), fetching userid: ${this.userid}`);
     await this.getUserDetails();
   }
 
   // @TODO: Implement wayPoints
-  trackRecord(track){
+  trackRecord(track) {
     let location = [];
-    location.push({lat: track.startPoint.lat.toString(), lng: track.startPoint.lng.toString()});
-    if(track.wayPoints.length > 0){
-      track.wayPoints.forEach( (obj, index) => {
-        if(index === obj.length-1){
-          location.push({lat: track.endPoint.lat.toString(), lng: track.endPoint.lng.toString()});
+    location.push({ lat: track.startPoint.lat.toString(), lng: track.startPoint.lng.toString() });
+    if (track.wayPoints.length > 0) {
+      track.wayPoints.forEach((obj, index) => {
+        if (index === obj.length - 1) {
+          location.push({ lat: track.endPoint.lat.toString(), lng: track.endPoint.lng.toString() });
         }
-        else{
-          location.push({lat: obj.lat.toString(), lng: obj.lng.toString()});
+        else {
+          location.push({ lat: obj.lat.toString(), lng: obj.lng.toString() });
         }
       })
       return location;
     }
 
-    else{
-      location.push({lat: track.endPoint.lat.toString(), lng: track.endPoint.lng.toString()});
+    else {
+      location.push({ lat: track.endPoint.lat.toString(), lng: track.endPoint.lng.toString() });
       return location;
     }
   }
 
-  getDifficulty(difficulty){
-    switch(difficulty){
+  getDifficulty(difficulty) {
+    switch (difficulty) {
       case 1:
-        return( <div style={{ display: 'inline-block' }}> <Icon color='yellow' name='star'/><Icon name='star'/><Icon name='star'/><Icon name='star'/><Icon name='star'/> </div>)
+        return (<div className='caseClass'> <Icon color='yellow' name='star' /><Icon name='star' /><Icon name='star' /><Icon name='star' /><Icon name='star' /> </div>)
         break;
 
       case 2:
-        return( <div style={{ display: 'inline-block' }}> <Icon color='yellow' name='star'/><Icon color='yellow' name='star'/><Icon name='star'/><Icon name='star'/><Icon name='star'/></div>)
+        return (<div className='caseClass'> <Icon color='yellow' name='star' /><Icon color='yellow' name='star' /><Icon name='star' /><Icon name='star' /><Icon name='star' /></div>)
         break;
 
       case 3:
-        return( <div style={{ display: 'inline-block' }}> <Icon color='yellow' name='star'/><Icon color='yellow' name='star'/><Icon color='yellow' name='star'/><Icon name='star'/><Icon name='star'/></div>)
+        return (<div className='caseClass'> <Icon color='yellow' name='star' /><Icon color='yellow' name='star' /><Icon color='yellow' name='star' /><Icon name='star' /><Icon name='star' /></div>)
         break;
 
       case 4:
-        return( <div style={{ display: 'inline-block' }}> <Icon color='yellow' name='star'/><Icon color='yellow' name='star'/><Icon color='yellow' name='star'/><Icon color='yellow' name='star'/><Icon name='star'/></div>)
+        return (<div className='caseClass'> <Icon color='yellow' name='star' /><Icon color='yellow' name='star' /><Icon color='yellow' name='star' /><Icon color='yellow' name='star' /><Icon name='star' /></div>)
         break;
 
       case 5:
-        return( <div style={{ display: 'inline-block' }}> <Icon color='yellow' name='star'/><Icon color='yellow' name='star'/><Icon color='yellow' name='star'/><Icon color='yellow' name='star'/><Icon color='yellow' name='star'/> </div>)
+        return (<div className='caseClass'> <Icon color='yellow' name='star' /><Icon color='yellow' name='star' /><Icon color='yellow' name='star' /><Icon color='yellow' name='star' /><Icon color='yellow' name='star' /> </div>)
         break;
 
       default:
-        return( <div style={{ display: 'inline-block' }}> <Icon name='star'/><Icon name='star'/><Icon name='star'/><Icon name='star'/><Icon name='star'/> </div>)
+        return (<div className='caseClass'> <Icon name='star' /><Icon name='star' /><Icon name='star' /><Icon name='star' /><Icon name='star' /> </div>)
         break;
     }
   }
 
   render() {
-    const userDetails = {...this.state.userDetails};
+    const userDetails = { ...this.state.userDetails };
     return (
       <div>
         <Menu2 currentPage={"Favorite Tracks"}></Menu2>
-        
+
         <Breadcrumb>
           <Breadcrumb.Item href="../">Login</Breadcrumb.Item>
           <Breadcrumb.Item href="../home">Home</Breadcrumb.Item>
@@ -116,96 +116,98 @@ class FavoriteTracks extends Component {
           </BLE>
         </Breadcrumb>
 
-        <Card.Group style={{ marginTop: '15px'}}>
-        {!this.state.loading && userDetails.favoriteTracks.length > 0 && userDetails.favoriteTracks.map( (track, index) => 
-          (
-            <React.Fragment key={index}>
-            
-            <Card style={{ margin: '0 auto', padding: '5px'}}>
-              <StaticGoogleMap
-                  maptype='roadmap'
-                  apiKey={getGoogleApiKey()}
-                  size="340x240"
-                  language='en'
-                >        
-                <Marker
-                  size ='mid'
-                  location={{ lat: track.startPoint.lat.toString(), lng: track.startPoint.lng.toString() }}
-                  color="green"
-                  label="A"
-                />
+        <Card.Group className='cardGroup'>
+          {!this.state.loading && userDetails.favoriteTracks.length > 0 && userDetails.favoriteTracks.map((track, index) =>
+            (
+              <React.Fragment key={index}>
 
-                <Marker
-                  size ='mid'
-                  location={{ lat: track.endPoint.lat.toString(), lng: track.endPoint.lng.toString() }}
-                  color="red"
-                  label="B"
-                />
-                <Path
-                  points={[
-                    this.trackRecord(track)
-                  ]}
-                />
-              </StaticGoogleMap> 
+                <Card className='cardClass'>
+                  <StaticGoogleMap
+                    maptype='roadmap'
+                    apiKey={getGoogleApiKey()}
+                    size="340x240"
+                    language='en'
+                  >
+                    <Marker
+                      size='mid'
+                      location={{ lat: track.startPoint.lat.toString(), lng: track.startPoint.lng.toString() }}
+                      color="green"
+                      label="A"
+                    />
 
-              <Card.Content>
-                <Card.Header> 
-                  {track.title.toString()} 
-                </Card.Header>
-                <Card.Meta>
-                <div>
-                  { this.getDifficulty(parseInt(track.difficultyLevel.star)) }
-                  <span className='date' style={{ float: 'right'}}>
-                      {track.travelMode.toString() === 'WALKING' ? <Icon title='Walking' name='male' color='blue' size='large'/> : <Icon name='bicycle' color='blue' size='large'/>}
-                  </span>
-                </div>
+                    <Marker
+                      size='mid'
+                      location={{ lat: track.endPoint.lat.toString(), lng: track.endPoint.lng.toString() }}
+                      color="red"
+                      label="B"
+                    />
+                    <Path
+                      points={[
+                        this.trackRecord(track)
+                      ]}
+                    />
+                  </StaticGoogleMap>
 
-                </Card.Meta>
+                  <Card.Content>
+                    <Card.Header>
+                      {track.title.toString()}
+                    </Card.Header>
+                    <Card.Meta>
+                      <div>
+                        {this.getDifficulty(parseInt(track.difficultyLevel.star))}
+                        <span className='date spanClass'>
+                          {track.travelMode.toString() === 'WALKING' ? <Icon title='Walking' name='male' color='blue' size='large' /> : <Icon name='bicycle' color='blue' size='large' />}
+                        </span>
+                      </div>
 
-                <Card.Description> 
-                  <Label.Group>
-                    <Label style={{ width: '55px'}} size='large' color='teal'>From</Label>
-                    <Label size='large'> {` ${track.startPoint.street ? track.startPoint.street + ', ' : ''} ${track.startPoint.city ? track.startPoint.city + '' : ''}`} </Label>
-                  </Label.Group>
-                  <Label.Group>
-                    <Label style={{ width: '55px'}} size='large' color='teal'>To</Label>
-                    <Label size='large'> {` ${track.endPoint.street ? track.endPoint.street + ', ' : ''} ${track.endPoint.city ? track.endPoint.city + '' : ''}`} </Label>
-                  </Label.Group>
-                  <Label.Group>
-                    <Label size='large' color='teal'>Duration</Label>
-                    <Label size='large'> {userDetails.accessibility === 1 ? track.nonDisabledTime.actual.toString() + ' minutes' : track.disabledTime.actual.toString() + ' minutes'} </Label>
-                  </Label.Group>                
-                  <Label.Group>
-                    <Label size='large' color='teal'>Distance</Label>
-                    <Label size='large'> {track.distance.toString() + 'm'} </Label>
-                  </Label.Group>                
-                  <Label.Group>
-                    <Label size='large' color='grey'>Description</Label>
-                    <Label size='large'> {track.description.toString()}  </Label>
-                  </Label.Group>                
-                  <FavoriteButton
-                  trackid={track._id}>
-                  </FavoriteButton>
-                </Card.Description>
-                
-              </Card.Content>
-              <Card.Content extra>
-              </Card.Content>
+                    </Card.Meta>
 
-              <NavLink to=
-              //navigate to TrackDetails via TemplateComponent with the params
-              {{pathname: `${process.env.PUBLIC_URL}/trackDetails`, 
-                idOfTrack: track._id}}>
-                <Button primary style={{width: '100%'}}>
-                Live Navigation
+                    <Card.Description>
+                      <Label.Group>
+                        <Label className='labelClass' size='large' color='teal'>From</Label>
+                        <Label size='large'> {` ${track.startPoint.street ? track.startPoint.street + ', ' : ''} ${track.startPoint.city ? track.startPoint.city + '' : ''}`} </Label>
+                      </Label.Group>
+                      <Label.Group>
+                        <Label className='labelClass' size='large' color='teal'>To</Label>
+                        <Label size='large'> {` ${track.endPoint.street ? track.endPoint.street + ', ' : ''} ${track.endPoint.city ? track.endPoint.city + '' : ''}`} </Label>
+                      </Label.Group>
+                      <Label.Group>
+                        <Label size='large' color='teal'>Duration</Label>
+                        <Label size='large'> {userDetails.accessibility === 1 ? track.nonDisabledTime.actual.toString() + ' minutes' : track.disabledTime.actual.toString() + ' minutes'} </Label>
+                      </Label.Group>
+                      <Label.Group>
+                        <Label size='large' color='teal'>Distance</Label>
+                        <Label size='large'> {track.distance.toString() + 'm'} </Label>
+                      </Label.Group>
+                      <Label.Group>
+                        <Label size='large' color='grey'>Description</Label>
+                        <Label size='large'> {track.description.toString()}  </Label>
+                      </Label.Group>
+                      <FavoriteButton
+                        trackid={track._id}>
+                      </FavoriteButton>
+                    </Card.Description>
+
+                  </Card.Content>
+                  <Card.Content extra>
+                  </Card.Content>
+
+                  <NavLink to=
+                    //navigate to TrackDetails via TemplateComponent with the params
+                    {{
+                      pathname: `${process.env.PUBLIC_URL}/trackDetails`,
+                      idOfTrack: track._id
+                    }}>
+                    <Button primary className='buttonClass'>
+                      Live Navigation
                 </Button>
-              </NavLink>
+                  </NavLink>
 
-            </Card>
-           
-            </React.Fragment>
-          ))
-        }
+                </Card>
+
+              </React.Fragment>
+            ))
+          }
         </Card.Group>
         {!this.state.loading && userDetails.favoriteTracks.length <= 0 && (<div> <p> Favorite tracks list is empty. </p> </div>)}
         {/* Reports:  {track.reports.map( (report) => return{<div> Report: {report.report} Reported by: {report.userid} </div>})} */}
@@ -214,4 +216,4 @@ class FavoriteTracks extends Component {
     )
   }
 }
-  export default FavoriteTracks;
+export default FavoriteTracks;
