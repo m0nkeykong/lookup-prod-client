bce5594e61d9732618d4c1ca46b38c590244dc27
m0nkeykong <haim1321@gmail.com>
2019-08-30 15:02:20 +0300

    ble css

--- src/Components/BLE.js
+++ src/Components/BLE.js
@@ -3,6 +3,7 @@ import BLEController from './BLEController';
 import { makeStyles } from '@material-ui/styles';
 // import { Icon, Button } from 'semantic-ui-react'
 import Icon from '@material-ui/core/Icon';
+import './style/BLE.css';
 
 // UI elements.
 // const deviceNameLabel = document.getElementById('device-name');
@@ -60,23 +61,25 @@ class BLE extends Component {
   // connect button functionallity (open available bluetooth device list)                  
   connectButton = () => {
     this.BLEController.connect()
-    .then((device) => {console.log('device connected')
-      this.setState({
-        isBLEConnected: true
-      });}
-    );
+      .then((device) => {
+        console.log('device connected')
+        this.setState({
+          isBLEConnected: true
+        });
+      }
+      );
   };
 
   // disconnect button functionallity (disconnet component)               
   disconnectButton = () => {
     this.BLEController.disconnect();
-    this.setState({ 
+    this.setState({
       isBLEConnected: false,
       showBLEnotification: false
-     });
+    });
   };
 
-  
+
 
   render() {
     const classes = (function () {
@@ -90,16 +93,16 @@ class BLE extends Component {
       }));
     });
     return (
-      <div style={{ position: 'absolute', right: '5px'}}>
+      <div className='mainDiv'>
         {!this.state.isBLEConnected ?
-        <div style={{ fontSize: '100%', lineHight: '100%'}}>
-            <Icon style={{ color: 'orange', fontSize: '20px' }} id="connect" onClick={this.connectButton} >bluetooth_disabled</Icon>
+          <div className='secDiv'>
+            <Icon className='disabledIcon' id="connect" onClick={this.connectButton} >bluetooth_disabled</Icon>
             {/* <Icon color='teal' size='big' name='bluetooth b' style={{ backgroundColor: 'light gray', borderRadius: '50%' }}/>Bluetooth */}
-        </div> :
-          <div style={{ fontSize: '100%', lineHight: '100%' }}>
-            <Icon style={{ color: 'green', fontSize: '20px' }} id="disconnect" onClick={this.disconnectButton}>bluetooth_connected</Icon>
-        </div>
-      }
+          </div> :
+          <div className='secDiv'>
+            <Icon className='enabledIcon' id="disconnect" onClick={this.disconnectButton}>bluetooth_connected</Icon>
+          </div>
+        }
       </div>
     );
   }
